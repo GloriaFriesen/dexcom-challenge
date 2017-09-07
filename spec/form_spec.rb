ef7c402155bd837b0dfbc2e4284a1b01ee131134
Gloria Friesen <gloriafriesen@gmail.com>
2017-09-07 11:57:25 -0700

    add test case A05

--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -1,7 +1,7 @@
 require('capybara/rspec')
 
 Capybara.default_driver = :selenium_chrome
-Capybara.app_host = 'https://shrouded-lowlands-66853.herokuapp.com'
+Capybara.app_host = 'https://shrouded-lowlands-66853.herokuapp.com/'
 Capybara.run_server = false
 
 # # A01
@@ -56,9 +56,40 @@ Capybara.run_server = false
 #   end
 # end
 
-#A04
-describe "Fill out all required fields with valid data and check all checkboxes. Do not select a suffix.", :type => :feature do
-  it "successfully registers new user with no errors" do
+# #A04
+# describe "Fill out all required fields with valid data and check all checkboxes. Do not select a suffix.", :type => :feature do
+#   it "successfully registers new user with no errors" do
+#     visit('/')
+#     fill_in 'user[first_name]', with: 'Tina'
+#     fill_in 'user[last_name]', with: 'Belcher'
+#     fill_in 'user[title]', with: 'Line Cook'
+#     fill_in 'user[email]', with: 'belchers@burgers.com'
+#     fill_in 'user[phone_number]', with: '347-867-5309'
+#     fill_in 'user[business_name]', with: 'Bobs Burgers'
+#     fill_in 'user[address_1]', with: '568 Ocean Ave'
+#     fill_in 'user[city]', with: 'Rockaway Beach'
+#     select 'NY', from: 'user[state]'
+#     fill_in 'user[zip]', with: '11693'
+#     check 'user[agree_1]'
+#     check 'user[agree_2]'
+#     expect(page).to have_field('user[first_name]', :with =>'Tina')
+#     expect(page).to have_field('user[last_name]', :with => 'Belcher')
+#     expect(page).to have_field('user[title]', :with => 'Line Cook')
+#     expect(page).to have_field('user[email]', :with => 'belchers@burgers.com')
+#     expect(page).to have_field('user[phone_number]', :with => '347-867-5309')
+#     expect(page).to have_field('user[business_name]', :with => 'Bobs Burgers')
+#     expect(page).to have_field('user[address_1]', :with => '568 Ocean Ave')
+#     expect(page).to have_field('user[zip]', :with => '11693')
+#     expect(page).to have_field("user[state]", :with =>"NY")
+#     expect(page).to have_field('user[agree_1]', checked: true)
+#     expect(page).to have_field('user[agree_2]', checked: true)
+#     page.save_screenshot('A04.png')
+#   end
+# end
+
+#A05
+describe "Select the save user button with required fields complete with valid data", :type => :feature do
+  it "navigates to success page with no error message indicating a suffix is required" do
     visit('/')
     fill_in 'user[first_name]', with: 'Tina'
     fill_in 'user[last_name]', with: 'Belcher'
@@ -72,19 +103,8 @@ describe "Fill out all required fields with valid data and check all checkboxes.
     fill_in 'user[zip]', with: '11693'
     check 'user[agree_1]'
     check 'user[agree_2]'
-
-    expect(page).to have_field('user[first_name]', :with =>'Tina')
-    expect(page).to have_field('user[last_name]', :with => 'Belcher')
-    expect(page).to have_field('user[title]', :with => 'Line Cook')
-    expect(page).to have_field('user[email]', :with => 'belchers@burgers.com')
-    expect(page).to have_field('user[phone_number]', :with => '347-867-5309')
-    expect(page).to have_field('user[business_name]', :with => 'Bobs Burgers')
-    expect(page).to have_field('user[address_1]', :with => '568 Ocean Ave')
-    expect(page).to have_field('user[zip]', :with => '11693')
-    expect(page).to have_field("user[state]", :with =>"NY")
-    expect(page).to have_field('user[agree_1]', checked: true)
-    expect(page).to have_field('user[agree_2]', checked: true)
-
-    page.save_screenshot('A04.png')
+    click_button 'Save User'
+    expect(page).to have_content 'Success! You have signed up. Register a user.'
+    page.save_screenshot('A05.png')
   end
 end
