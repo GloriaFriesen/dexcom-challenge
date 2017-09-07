7666277dddb4b4f54c6a5bf38a23f9d258492dc4
Gloria Friesen <gloriafriesen@gmail.com>
2017-09-06 19:58:10 -0700

    complete test cases A01 and A02

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source('https://rubygems.org')
 gem('rspec')
 gem('capybara')
 gem('pry')
+
+gem('selenium-webdriver', '~> 2.53', '>= 2.53.4')
---
+++ spec/form_spec.rb
@@ -0,0 +1,35 @@
+require('capybara/rspec')
+
+Capybara.default_driver = :selenium_chrome
+Capybara.app_host = 'https://shrouded-lowlands-66853.herokuapp.com'
+Capybara.run_server = false
+
+# A01
+describe "Navigate to the testing page", :type => :feature do
+  it "Page to register a new user is displayed." do
+    visit ('/')
+    expect(page).to have_content 'Register New User'
+    page.save_screenshot('A01.png')
+  end
+end
+
+# A02
+describe "Fill out required fields except first name with valid data and check all checkboxes. Select the save user button.", :type => :feature do
+  it "displays error message indicating first name requires a value" do
+    visit('/')
+    fill_in 'user[last_name]', with: 'Belcher'
+    fill_in 'user[title]', with: 'Line Cook'
+    fill_in 'user[email]', with: 'belchers@burgers.com'
+    fill_in 'user[phone_number]', with: '347-867-5309'
+    fill_in 'user[business_name]', with: 'Bobs Burgers'
+    fill_in 'user[address_1]', with: '568 Ocean Ave'
+    fill_in 'user[city]', with: 'Rockaway Beach'
+    select 'NY', from: 'user[state]'
+    fill_in 'user[zip]', with: '11693'
+    check 'user[agree_1]'
+    check 'user[agree_2]'
+    click_button 'Save User'
+    expect(page).to have_content'First Name cannot be blank'
+    page.save_screenshot('A02.png')
+  end
+end
