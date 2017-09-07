9905d363d29c213b1e49ab171632d651e4cfbaef
Gloria Friesen <gloriafriesen@gmail.com>
2017-09-06 20:38:11 -0700

    add A03 test case

--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -33,3 +33,25 @@ describe "Fill out required fields except first name with valid data and check a
     page.save_screenshot('A02.png')
   end
 end
+
+# A03
+describe "Fill out all required fields with valid data and check all checkboxes. Attempt to fill in the first name field with 16 valid characters", :type => :feature do
+  it "Only the first 15 characters are entered in first name field" do
+    visit ('/')
+    fill_in 'user[last_name]', with: 'Belcher'
+    fill_in 'user[title]', with: 'Line Cook'
+    fill_in 'user[email]', with: 'belchers@burgers.com'
+    fill_in 'user[phone_number]', with: '347-867-5309'
+    fill_in 'user[business_name]', with: 'Bobs Burgers'
+    fill_in 'user[address_1]', with: '568 Ocean Ave'
+    fill_in 'user[city]', with: 'Rockaway Beach'
+    select 'NY', from: 'user[state]'
+    fill_in 'user[zip]', with: '11693'
+    check 'user[agree_1]'
+    check 'user[agree_2]'
+    #enter word with 16 characters (Programmatically) in First Name field
+    fill_in 'user[first_name]', with: 'Programmatically'
+    expect(page).to have_field('user[first_name]', :with =>'Programmaticall')
+    page.save_screenshot('A03.png')
+  end
+end
